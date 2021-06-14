08fcaf3c8ca3b0c30dc4f960afe33385546a7bfd
Junaid-Rasheed <junaidrasheed300@gmail.com>
2021-06-15 03:30:08 +0500

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
 import React from "react"
+import { useEffect } from "react"
+import { useState } from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [data, setdata] = useState("")
+
+  const working = async () => {
+    const work = await fetch(".netlify/functions/hello")
+    const response = await work.json()
+    setdata(response)
+  }
+  useEffect(async () => {
+    working()
+  }, [])
+
+  return (
+    <div>
+      Hello {data.message}!<br />
+      <button onClick={working}>Change Name </button>
+    </div>
+  )
 }
